9a6dd365b214b2e202fb3b3f3865e36fe5dd06b5
Matheus Ventura Nellessen <ti.matheus.v.n@gmail.com>
2026-03-12 15:08:12 -0300

    Adiciona 01_create_database.sql

---
+++ sql/00_setup/01_create_database.sql
@@ -0,0 +1,7 @@
+CREATE DATABASE brazilian_ecommerce_by_olist
+    WITH
+    OWNER = postgres
+    ENCODING = 'UTF8'
+    LOCALE_PROVIDER = 'libc'
+    CONNECTION LIMIT = -1
+    IS_TEMPLATE = False;
